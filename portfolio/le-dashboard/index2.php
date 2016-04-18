26f17ffb7a58bb0640afb7bafa613eeee93f4bfd
Danielle Fortier <dfortier@localedge.com>
2016-04-18 17:16:21 -0400

    New port

---
+++ portfolio/le-dashboard/index2.php
@@ -0,0 +1,266 @@
+<?php require_once("../../resources/templates/header-portfolio.php"); ?>
+
+<body class="portfolio">
+
+     <!-- Header -->
+     <?php require_once("../../resources/templates/port-nav.php"); ?>
+
+     <!-- Welcome Grid Section -->
+    <section class="blue-bg" id="title">
+    <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Localedge Customer Dashboard</h1>
+                    <p class="intro">In 2015, Localedge completely changed the look and feel of their customer dashboard experience from dated and impractical to modernized and user-friendly. </p>
+                    <blockquote><p>This is a quote about how awesome the dashboard is. Boom. -- Satisfied customer</p></blockquote>
+                </div>
+            </div>
+        </div>
+    
+    </section>
+    
+    
+    <!-- What Is Section -->
+    <section class="gray-bg" id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <img class="img-responsive" src="../../resources/img/portfolio/le-dash/macbook2.png" alt="">
+                </div>
+                <div class="col-md-4 content-mid">
+                    <h3>What it is</h3>
+                    
+                    <p class="macro">The customer dashboard is a portal where users login to manage their products and view their reporting through the applications they have added to their personal campaigns.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- My Role Section -->
+    <section class="cover-1" id="myrole">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 content-bottom">
+                    <h3>My Role</h3>
+                    
+                    <p class="macro">I lead the design of the customer dashboard. The previous design was severly out of date, in 2014. My job was to modernize the interface and to address customer pain-points related to the product management and reporting experiences.</p>
+                </div>
+                <div class="col-md-6">
+                    <!--<img class="img-responsive" src="../../resources/img/portfolio/le-dash/psd/my-role.png" alt="">-->
+                </div>
+            </div>
+          
+            </div>
+     
+    </section>
+    <section class="white-bg" id="myrole2">
+        <div class="container">
+            <div class="row">
+                 <div class="col-md-4">
+                    <h4>Customer Insights & Ideation</h4>
+                    <p class="micro">I collaborated with three product specialists to uncover insights and translate concepts into features that address customer behaviors and issues.</p>
+                </div>
+                <div class="col-md-4">
+                    <h4>Strategy & Vision</h4>
+                    <p class="micro">I created wireframes, mockups and prototypes to share the vision, design principles and content strategy. This helped spark conversation for new and different ideas, and drive decision making.  </p>
+                </div>
+                <div class="col-md-4">
+                    <h4>Planning & Scope</h4>
+                    <p class="micro">I worked with product team members to define each product and its' purpose. We balanced both customer goals and business goals. I negotiated and helped prioritize new features for launch and beyond.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>Direction & Coordination</h4>
+                    <p class="micro">I collaborated with developers and worked with them on the finalized design direction using CSS and HTML standards. </p>
+                </div>
+                <div class="col-md-4">
+                    <h4>Design Execution</h4>
+                    <p class="micro">I designed and executed wireframes, mockups, prototypes and design specs, which became finals and resulted in what is live on the dashboard today. </p>
+                </div>
+                <!--<div class="col-md-4">
+                    <h3>Customer Insights & Ideation</h3>
+                    <p>I lead the design of the customer dashboard. The previous design was severly out of date, in 2014. My job was to modernize the interface and to address customer pain-points related to the product management and reporting experiences.</p>
+                </div>-->
+            </div>
+        </div>
+     
+    </section>
+    
+
+    <!-- What Is Section -->
+    <section class="gray-bg" id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <img class="img-responsive" src="../../resources/img/portfolio/le-dash/macbook2.png" alt="">
+                </div>
+                <div class="col-md-4 content-mid">
+                    <h3>What it is</h3>
+                    
+                    <p>The customer dashboard is a portal where users login to manage their products and view their reporting through the applications they have added to their personal campaigns.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="text-center">
+     
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        Copyright &copy; Danielle Fortier 2016
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visible-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+    <!-- Portfolio Modals -->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Customer Dashboard Redesign</h2>
+                            <hr class="star-primary">
+                            <img src="resources/img/portfolio/front/LE-dashboard1.png" class="img-responsive img-centered" alt="">
+                            <p>Localedge's customer dashboard design was severly out of date, in 2014. My job was to take an old design and modernize the interface and the user experience up to modern standards. Key pieces to my design concept were a side navigation to access an excessive list of internal links, tabbed individual reporting navigation for each application element, and an overall clean and consistant look and feel. The image above is just a basic example of what most of the reporting pages look like. </p>
+                            <a href="portfolio/le-dashboard/"><button class="btn btn-success">Project Case Studies</button></a>
+                            <ul class="list-inline item-details">
+                                <li>Client:
+                                    <strong>Localedge
+                                    </strong>
+                                </li>
+                                <li>Date:
+                                    <strong>May 2014 - Present
+                                    </strong>
+                                </li>
+                                <li>Services:
+                                    <strong>UX, HTML, CSS
+                                    </strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Project Title</h2>
+                            <hr class="star-primary">
+                            <img src="resources/img/portfolio/front/STRDS-LE.png" class="img-responsive img-centered" alt="">
+                            <p>With the redesign of the new Localedge customer dashboard, I felt there needed to be a new standards site of set design rules. This is still a work in progress. This site has been a true learning experience in my development as a UX/UI Designer as I was able to take ownership over ensuring these rules are followed and are communicated well to Product Owners and Developers. Setting design rules for a large application has been both challenging and rewarding. </p>
+                            <ul class="list-inline item-details">
+                                <li>Client:
+                                    <strong>Localedge
+                                    </strong>
+                                </li>
+                                <li>Link:
+                                    <strong><a href="http://uxui.localedge.com/ui-standards/">UX/UI Portal</a>
+                                    </strong>
+                                </li>
+                                <li>Date:
+                                    <strong>September 2015 - Present
+                                    </strong>
+                                </li>
+                                <li>Services:
+                                    <strong>Wordpress, HTML, CSS, JS, Bootstrap
+                                    </strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <div class="modal-body">
+                            <h2>Project Title</h2>
+                            <hr class="star-primary">
+                            <img src="resources/img/portfolio/front/MKPS-LE.png" class="img-responsive img-centered" alt="">
+                            <p>Use this area of the page to describe your project. The icon above is part of a free icon set by <a href="https://sellfy.com/p/8Q9P/jV3VZ/">Flat Icons</a>. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!</p>
+                            <ul class="list-inline item-details">
+                                <li>Client:
+                                    <strong>Localedge
+                                    </strong>
+                                </li>
+                                <li>Date:
+                                    <strong>June 2015
+                                    </strong>
+                                </li>
+                                <li>Services:
+                                    <strong>Wordpress, HTML, CSS, JS, Bootstrap, Illustrator
+                                    </strong>
+                                </li>
+                            </ul>
+                            <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+ 
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/cbpAnimatedHeader.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/freelancer.js"></script>
+
+</html>
